0fbc3c8a01653b8e63035b5f2012fe19bf0df393
mcuomo <michael.r.cuomo@gmail.com>
2025-01-25 00:20:54 -0500

    Iterative Approach

--- csharp/Sieve/Sieve.cs
+++ csharp/Sieve/Sieve.cs
@@ -11,8 +11,54 @@ public class SieveImplementation : ISieve
     private List<long> primes = new List<long> { 2 };
 
 
+    private long newMaxGenerated = 2;
+    private List<List<long>> PrimeFactors = new List<List<long>> { new List<long> { 2 } };
+    private HashSet<long> PrimeFactorsSet = new HashSet<long> { 2 };
 
     public long NthPrime(long n)
+    {
+        if (PrimeFactors.Count > n)
+        {
+            return PrimeFactors[(int)n][0];
+        }
+        else
+        {
+            while (PrimeFactors.Count <= n)
+            {
+                for (var x = newMaxGenerated + 1; x < newMaxGenerated + 1000; x++)
+                {
+                    if (PrimeFactorsSet.Contains(x))
+                    {
+                        continue;
+                    }
+                    //var maxCheck = Math.Sqrt(x);
+                    for (var i = 0; i < PrimeFactors.Count; i++)
+                    {
+                        var currentMax = PrimeFactors[i][PrimeFactors[i].Count - 1];
+                        while (currentMax <= x)
+                        {
+                            currentMax = PrimeFactors[i][PrimeFactors[i].Count - 1] + PrimeFactors[i][0];
+                            if (!PrimeFactorsSet.Contains(currentMax))
+                            {
+                                PrimeFactorsSet.Add(currentMax);
+                            }
+                            PrimeFactors[i].Add(currentMax);
+                        }
+                    }
+                    if (!PrimeFactorsSet.Contains(x))
+                    {
+                        PrimeFactorsSet.Add(x);
+                        PrimeFactors.Add(new List<long> { x });
+                    }
+                }
+                newMaxGenerated += 1000;
+            }
+            return PrimeFactors[(int)n][0];
+        }
+    }
+
+
+    public long NthPrime2(long n)
     {
         if (primes.Count > n)
         {
@@ -24,7 +70,7 @@ public class SieveImplementation : ISieve
             while (primes.Count <= n)
             {
                 var layeredContinue = false;
-                for (var i = maxGenerated + 1; i < maxGenerated + 10000; i++)
+                for (var i = maxGenerated + 1; i < maxGenerated + 100000; i++)
                 {
                     var maxCheck = Math.Sqrt(i);
                     for (var j = 0; j < primes.Count; j++)
@@ -46,7 +92,7 @@ public class SieveImplementation : ISieve
                     }
                     primes.Add(i);
                 }
-                maxGenerated += 10000;
+                maxGenerated += 100000;
             }
             return primes[(int)n];
         }
