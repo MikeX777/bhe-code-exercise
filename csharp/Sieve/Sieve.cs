46215ae8089452493c94c95d20bfb50db0914382
mcuomo <michael.r.cuomo@gmail.com>
2025-01-26 11:43:22 -0500

    Adding Long Support

--- csharp/Sieve/Sieve.cs
+++ csharp/Sieve/Sieve.cs
@@ -9,10 +9,13 @@ public interface ISieve
 
 public class SieveImplementation : ISieve
 {
-    private long maxGenerated = 2;
+    private long maxGenerated = 3;
     private List<long> primes = new List<long> { 2 };
 
-    private long newMaxGenerated = 2;
+    private Dictionary<uint, List<long>> primeDictionary = new() { { 0, new List<long> { 2 } }};
+    private uint maxDictionary = 0;
+
+private long newMaxGenerated = 2;
     private List<List<long>> PrimeFactors = new() { new List<long> { 2 } };
     private HashSet<long> PrimeFactorsSet = new() { 2 };
     private ConcurrentDictionary<long, bool> PrimeFactorsDictionary = new(10000000, Environment.ProcessorCount * 2) { };
@@ -75,7 +78,103 @@ public class SieveImplementation : ISieve
     }
 
 
+
+
     public long NthPrime(long n)
+    {
+        if (n < 0)
+        {
+            return -1L;
+        }
+        var quotient = (uint)(n / uint.MaxValue);
+        var remainder = (int)n % int.MaxValue;
+        while (maxDictionary < quotient)
+        {
+            for (int i = primeDictionary[maxDictionary].Count; i <= int.MaxValue; i++)
+            {
+                var primeFound = false;
+                do
+                {
+                    var increment = false;
+                    var maxCheck = Math.Sqrt(maxGenerated);
+                    for (uint j = 0; j <= maxDictionary; j++)
+                    {
+                        for (var k = 0; k < int.MaxValue; k++)
+                        {
+                            if (maxGenerated % primeDictionary[j][k] == 0)
+                            {
+                                increment = true;
+                                break;
+                            }
+                            if (primeDictionary[j][k] > maxCheck)
+                            {
+                                primeFound = true;
+                                break;
+                            }
+                        }
+                        if (increment || primeFound)
+                        {
+                            break;
+                        }
+                    }
+                    if (increment)
+                    {
+                        maxGenerated++;
+                    }
+                } while (!primeFound);
+                primeDictionary[maxDictionary].Add(maxGenerated);
+                maxGenerated++;
+            }
+            maxDictionary++;
+            primeDictionary[maxDictionary] = new List<long>();
+        }
+        if (primeDictionary.Count > quotient && primeDictionary[quotient].Count > remainder)
+        {
+            return primeDictionary[quotient][remainder];
+        }
+        else
+        {
+            for (int i = primeDictionary[maxDictionary].Count; i <= remainder; i++)
+            {
+                var primeFound = false;
+                do
+                {
+                    var increment = false;
+                    var maxCheck = Math.Sqrt(maxGenerated);
+                    for (uint j = 0; j <= maxDictionary; j++)
+                    {
+                        for (var k = 0; k < int.MaxValue; k++)
+                        {
+                            if (maxGenerated % primeDictionary[j][k] == 0)
+                            {
+                                increment = true;
+                                break;
+                            }
+                            if (primeDictionary[j][k] > maxCheck)
+                            {
+                                primeFound = true;
+                                break;
+                            }
+                        }
+                        if (increment || primeFound)
+                        {
+                            break;
+                        }
+                    }
+                    if (increment)
+                    {
+                        maxGenerated++;
+                    }
+                } while (!primeFound);
+                primeDictionary[maxDictionary].Add(maxGenerated);
+                maxGenerated++;
+            }
+            return primeDictionary[quotient][remainder]; 
+        } 
+    }
+
+
+    public long NthPrimeNoLong(long n)
     {
         if (primes.Count > n)
         {
@@ -86,7 +185,6 @@ public class SieveImplementation : ISieve
             while (primes.Count <= n)
             {
                 var layeredContinue = false;
-                var primeFound = false;
                 for (var i = maxGenerated + 1; i < maxGenerated + 100000; i++)
                 {
                     var maxCheck = Math.Sqrt(i);
