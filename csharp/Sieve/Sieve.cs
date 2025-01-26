7848e3214e8f2874c0dc359e6d88bb1cafe418af
mcuomo <michael.r.cuomo@gmail.com>
2025-01-26 09:48:03 -0500

    Adjusted Parallelization

--- csharp/Sieve/Sieve.cs
+++ csharp/Sieve/Sieve.cs
@@ -1,4 +1,6 @@
-﻿namespace Sieve;
+﻿using System.Collections.Concurrent;
+
+namespace Sieve;
 
 public interface ISieve
 {
@@ -10,12 +12,17 @@ public class SieveImplementation : ISieve
     private long maxGenerated = 2;
     private List<long> primes = new List<long> { 2 };
 
-
     private long newMaxGenerated = 2;
-    private List<List<long>> PrimeFactors = new List<List<long>> { new List<long> { 2 } };
-    private HashSet<long> PrimeFactorsSet = new HashSet<long> { 2 };
+    private List<List<long>> PrimeFactors = new() { new List<long> { 2 } };
+    private HashSet<long> PrimeFactorsSet = new() { 2 };
+    private ConcurrentDictionary<long, bool> PrimeFactorsDictionary = new(10000000, Environment.ProcessorCount * 2) { };
 
-    public long NthPrime2(long n)
+    public SieveImplementation()
+    {
+        PrimeFactorsDictionary.TryAdd(2, true);
+    }
+
+    public long NthPrimeFactor(long n)
     {
         if (PrimeFactors.Count > n)
         {
@@ -32,28 +39,34 @@ public class SieveImplementation : ISieve
                         continue;
                     }
                     //var maxCheck = Math.Sqrt(x);
-                    var sync = new object();
-                    Parallel.For(0, PrimeFactors.Count, i =>
+                    //var sync = new object();
+                    Parallel.ForEach(PrimeFactors, p =>
                     {
-                        var currentMax = PrimeFactors[i][PrimeFactors[i].Count - 1];
+                        var currentMax = p[p.Count - 1];
                         while (currentMax <= x)
                         {
-                            currentMax = PrimeFactors[i][PrimeFactors[i].Count - 1] + PrimeFactors[i][0];
-                            lock (sync)
-                            {
-                                if (!PrimeFactorsSet.Contains(currentMax))
-                                {
-                                    PrimeFactorsSet.Add(currentMax);
-                                }
-                                PrimeFactors[i].Add(currentMax);
-                            }
+                            currentMax = p[p.Count - 1] + p[0];
+                            //lock (sync)
+                            //{
+                            PrimeFactorsDictionary.TryAdd(currentMax, false);
+                            p.Add(currentMax);
+                            //if (!PrimeFactorsSet.Contains(currentMax))
+                            //    {
+                            //        PrimeFactorsSet.Add(currentMax);
+                            //    }
+                            //    PrimeFactors[i].Add(currentMax);
+                            //}
                         }
                     });
-                    if (!PrimeFactorsSet.Contains(x))
+                    if (PrimeFactorsDictionary.TryAdd(x, true))
                     {
-                        PrimeFactorsSet.Add(x);
                         PrimeFactors.Add(new List<long> { x });
                     }
+                    //    if (!PrimeFactorsSet.Contains(x))
+                    //{
+                    //    PrimeFactorsSet.Add(x);
+                    //    PrimeFactors.Add(new List<long> { x });
+                    //}
                 }
                 newMaxGenerated += 1000;
             }
@@ -70,10 +83,10 @@ public class SieveImplementation : ISieve
         }
         else
         {
-            var maxPrime = Math.Sqrt(n);
             while (primes.Count <= n)
             {
                 var layeredContinue = false;
+                var primeFound = false;
                 for (var i = maxGenerated + 1; i < maxGenerated + 100000; i++)
                 {
                     var maxCheck = Math.Sqrt(i);
@@ -95,6 +108,45 @@ public class SieveImplementation : ISieve
                         continue;
                     }
                     primes.Add(i);
+                }
+                maxGenerated += 100000;
+            }
+            return primes[(int)n];
+        }
+    }
+
+
+    public long NthPrime2(long n)
+    {
+        if (primes.Count > n)
+        {
+            return primes[(int)n];
+        }
+        else
+        {
+            while (primes.Count <= n)
+            {
+                for (var i = maxGenerated + 1; i < maxGenerated + 100000; i++)
+                {
+                    var primeModFound = false;
+                    var maxCheck = Math.Sqrt(i);
+                    Parallel.For(0, primes.Count, (j, state) =>
+                    {
+
+                        if (i % primes[j] == 0)
+                        {
+                            primeModFound = true;
+                            state.Break();
+                        }
+                        if (primes[j] > maxCheck)
+                        {
+                            state.Break();
+                        }
+                    });
+                    if (!primeModFound)
+                    {
+                        primes.Add(i);
+                    }
                 }
                 maxGenerated += 100000;
             }
